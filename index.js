ac0c5bf97e7f6b93855c75b35d67caf6183dfc71
hasansjc <hasansjc@gmail.com>
2021-10-03 17:46:15 +0530

    changing port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http=require('http');
 const fs=require('fs');
-const port=process.env.PORT || 3000;
+const port=process.env.port || 3000;
 var requests=require('requests');
 
 const homeData=fs.readFileSync('home.html','utf-8');
